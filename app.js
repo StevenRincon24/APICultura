fdf9bb9acc00458848a3407afbba89688415e1b8
hovar.rincon@uptc.edu.co <hovar.rincon@uptc.edu.co>
2025-04-18 01:42:51 -0500

    Refactor: Cors All

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin:[ "http://localhost:5173", "https://cultura-nobsa.vercel.app"], credentials: true }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
